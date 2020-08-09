82df506af73f85d9bff381a57d4ebcfa3c8f36a7
Naghmeh <naghmeh.majidpour@gmail.com>
2020-08-09 16:05:02 -0400

    Initial commit of todo component

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+import { Todo } from "src/app/models/Todo";
+
+@Component({
+  selector: "app-todos",
+  templateUrl: "./todos.component.html",
+  styleUrls: ["./todos.component.css"],
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
